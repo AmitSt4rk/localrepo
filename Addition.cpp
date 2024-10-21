0dfed4e1b94af72362b389435f7c983d6e6aaadf
AmitSt4rk <ak87890693@gmail.com>
2024-10-21 16:38:41 +0530

    Initial files

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter First Number: ";
+    cin>>a;
+    cout<<"Enter Second Number: ";
+    cin>>b;
+    cout<<"Addition of Those two Value: "<<(a+b)<<endl;
+    return 0;
+}
